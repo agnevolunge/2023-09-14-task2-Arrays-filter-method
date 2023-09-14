2c9c3d53f925e318d081db3f7570efe6dadaf3f8
agnevolunge <agne.cepaite@gmail.com>
2023-09-14 14:01:05 +0300

    40 done

--- task.js
+++ task.js
@@ -16,212 +16,284 @@ let filteredElements1 = originalElements.filter(function(element) {
 
     return element >= 0
 })
-
 console.log(filteredElements1)
-
 
 // 3.2. Gauti tik neigiamus skaičius.
 let filteredElements2 = originalElements.filter(function(element) {
-
     console.log(element < 0)
-
     return element < 0
 })
-
 console.log(filteredElements2)
-
 
 // 3.3. Gauti tik skaičius, kurie dalinasi iš 2.
 let filteredElements3 = originalElements.filter(function(element) {
-  
     console.log(element % 2 === 0)
-
     return element % 2 === 0
 })
-
 console.log(filteredElements3)
 
 // 3.4. Gauti tik skaičius, kurie dalinasi iš 3.
 let filteredElements4 = originalElements.filter(function(element) {
-  
     console.log(element % 3 === 0)
-
     return element % 3 === 0
 })
-
 console.log(filteredElements4)
 
 // 3.5. Gauti tik skaičius, kurie dalinasi iš 5.
 
 let filteredElements5 = originalElements.filter(function(element) {
-  
     console.log(element % 5 === 0)
-
     return element % 5 === 0
 })
-
 console.log(filteredElements5)
 
 // 3.6. Gauti tik skaičius, kurie dalinasi iš 11.
 let filteredElements6 = originalElements.filter(function(element) {
-  
     console.log(element % 11 === 0)
-
     return element % 11 === 0
 })
-
 console.log(filteredElements6)
 
 // 3.7. Gauti tik skaičius, kurie dalinasi iš 31.
 
 let filteredElements7 = originalElements.filter(function(element) {
-  
     console.log(element % 31 === 0)
-
     return element % 31 === 0
 })
-
 console.log(filteredElements7)
 
 // 3.8. Gauti tik skaičius, kurie dalinasi iš 2 ir iš 3.
 
 let filteredElements8 = originalElements.filter(function(element) {
-  
     console.log(element % 2 === 0 && element % 3 === 0)
-
     return element % 2 === 0 && element % 3 === 0
 })
-
 console.log(filteredElements8)
 
 // 3.9. Gauti tik skaičius, kurie dalinasi iš 3 ir iš 7.
 
 let filteredElements9 = originalElements.filter(function(element) {
-
     return element % 3 === 0 && element % 7 === 0
 })
-
 console.log(filteredElements9)
 
 // 3.10. Gauti tik skaičius, kurie dalinasi iš 5 ir iš 9.
 
 let filteredElements10 = originalElements.filter(function(element) {
-
     return element % 5 === 0 && element % 9 === 0
 })
-
 console.log(filteredElements10)
 
 // 3.11. Gauti tik skaičius, kurie dalinasi iš 5 ir iš 11.
 
 let filteredElements11 = originalElements.filter(function(element) {
-
     return element % 5 === 0 && element % 11 === 0
 })
-
 console.log(filteredElements11)
 
 // 3.12. Gauti tik skaičius, kurie dalinasi iš 2, 8 ir 12.
 
 let filteredElements12 = originalElements.filter(function(element) {
-
     return element % 2 === 0 && element % 8 === 0 && element % 12 === 0
 })
-
 console.log(filteredElements12)
 
 // 3.13. Gauti tik skaičius, kurie dalinasi iš 2 arba iš 3.
 
 let filteredElements13 = originalElements.filter(function(element) {
-
     return element % 2 === 0 || element % 3 === 0 
 })
-
 console.log(filteredElements13)
 
 // 3.14. Gauti tik skaičius, kurie dalinasi iš 3 arba iš 5.
 
 let filteredElements14 = originalElements.filter(function(element) {
-
     return element % 3 === 0 || element % 5 === 0 
 })
-
 console.log(filteredElements14)
 
 // 3.15. Gauti tik skaičius, kurie dalinasi iš 5 arba iš 6.
 
 let filteredElements15 = originalElements.filter(function(element) {
-
     return element % 5 === 0 || element % 6 === 0 
 })
-
 console.log(filteredElements15)
 
 // 3.16. Gauti tik skaičius, kurie dalinasi iš 7 arba iš 8.
 
 let filteredElements16 = originalElements.filter(function(element) {
-
     return element % 7 === 0 || element % 8 === 0 
 })
-
 console.log(filteredElements16)
 
 // 3.17. Gauti tik skaičius, kurie dalinasi iš 9 arba iš 13.
 
 let filteredElements17 = originalElements.filter(function(element) {
-
     return element % 9 === 0 || element % 13 === 0 
 })
-
 console.log(filteredElements17)
 
 // 3.18. Gauti tik skaičius, kurie dalinasi iš 2, 3 arba iš 5.
 
 let filteredElements18 = originalElements.filter(function(element) {
-
     return element % 2 === 0 || element % 3 === 0 || element % 5 === 0
 })
-
 console.log(filteredElements18)
 
 // 3.19. Gauti tik skaičius, kurie dalinasi iš 5, 7 arba iš 9.
 
 let filteredElements19 = originalElements.filter(function(element) {
-
     return element % 5 === 0 || element % 7 === 0 || element % 9 === 0
 })
-
 console.log(filteredElements19)
 
 // 3.20. Gauti tik skaičius, kurie dalinasi iš 7, 8 arba iš 11.
 
 let filteredElements20 = originalElements.filter(function(element) {
-
     return element % 7 === 0 || element % 8 === 0 || element % 11 === 0
 })
-
 console.log(filteredElements20)
 
 // 3.21. Gauti tik skaičius, kurie dalinasi iš 9, 12 arba iš 13.
+
+let filteredElements21 = originalElements.filter(function(element) {
+    return element % 9 === 0 || element % 12 === 0 || element % 13 === 0
+})
+console.log(filteredElements21)
+
 // 3.22. Gauti tik skaičius, kurie yra didesni už 100.
+
+let filteredElements22 = originalElements.filter(function(element) {
+    return element > 100
+})
+console.log(filteredElements22)
+
 // 3.23. Gauti tik skaičius, kurie yra didesni už 555.
+
+let filteredElements23 = originalElements.filter(function(element) {
+    return element > 555
+})
+console.log(filteredElements23)
+
 // 3.24. Gauti tik skaičius, kurie yra didesni arba lygūs 888.
+
+let filteredElements24 = originalElements.filter(function(element) {
+    return element >= 888
+})
+console.log(filteredElements24)
+
 // 3.25. Gauti tik skaičius, kurie yra didesni arba lygūs 6789.
+
+let filteredElements25 = originalElements.filter(function(element) {
+    return element >= 6789
+})
+console.log(filteredElements25)
+
 // 3.26. Gauti tik skaičius, kurie yra mažesni už 50.
+
+let filteredElements26 = originalElements.filter(function(element) {
+    return element < 50
+})
+console.log(filteredElements26)
+
 // 3.27. Gauti tik skaičius, kurie yra mažesni už 1000.
+
+let filteredElements27 = originalElements.filter(function(element) {
+    return element < 1000
+})
+console.log(filteredElements27)
+
 // 3.28. Gauti tik skaičius, kurie yra mažesni arba lygūs -1.
+
+let filteredElements28 = originalElements.filter(function(element) {
+    return element <= -1
+})
+console.log(filteredElements28)
+
 // 3.29. Gauti tik skaičius, kurie yra mažesni arba lygūs -5564.
+
+let filteredElements29 = originalElements.filter(function(element) {
+    return element <= -5564
+})
+console.log(filteredElements29)
+
 // 3.30. Gauti tik skaičius, kurie yra mažesni už 1000 ir didesni už 500.
+
+let filteredElements30 = originalElements.filter(function(element) {
+    return element < 1000 && element > 500
+})
+console.log(filteredElements30)
+
 // 3.31. Gauti tik skaičius, kurie yra mažesni už 100 ir didesni už 0.
+
+let filteredElements31 = originalElements.filter(function(element) {
+    return element < 100 && element > 0
+})
+console.log(filteredElements31)
+
 // 3.32. Gauti tik skaičius, kurie yra mažesni už 0 ir didesni už -50.
+
+let filteredElements32 = originalElements.filter(function(element) {
+    return element < 0 && element > -50
+})
+console.log(filteredElements32)
+
 // 3.33. Gauti tik skaičius, kurie yra mažesni arba lygūs 0 ir didesni už -100.
+
+let filteredElements33 = originalElements.filter(function(element) {
+    return element <= 0 && element > -100
+})
+console.log(filteredElements33)
+
 // 3.34. Gauti tik skaičius, kurie yra didesni arba lygūs 0 ir mažesni už 55.
+
+let filteredElements34 = originalElements.filter(function(element) {
+    return element >= 0 && element < 55
+})
+console.log(filteredElements34)
+
 // 3.35. Gauti tik skaičius, kurie yra didesni arba lygūs 444 ir mažesni arba lygūs 654.
+
+let filteredElements35 = originalElements.filter(function(element) {
+    return element >= 444 && element <= 654
+})
+console.log(filteredElements35)
+
 // 3.36. Gauti tik teigiamus skaičius, kurie dalinasi iš 2.
+
+let filteredElements36 = originalElements.filter(function(element) {
+    return element > 0  && element % 2 === 0
+})
+console.log(filteredElements36)
+
 // 3.37. Gauti tik teigiamus skaičius, kurie dalinasi iš 3.
+
+let filteredElements37 = originalElements.filter(function(element) {
+    return element > 0  && element % 3 === 0
+})
+console.log(filteredElements37)
+
 // 3.38. Gauti tik neigiamus skaičius, kurie dalinasi iš 4.
+
+let filteredElements38 = originalElements.filter(function(element) {
+    return element < 0  && element % 4 === 0
+})
+console.log(filteredElements38)
+
 // 3.39. Gauti tik neigiamus skaičius, kurie dalinasi iš 111.
+
+let filteredElements39 = originalElements.filter(function(element) {
+    return element < 0  && element % 111 === 0
+})
+console.log(filteredElements39)
+
 // 3.40. Gauti tik skaičius, kurie yra didesni už 500 ir kurie dalinasi iš 2.
+
+let filteredElements40 = originalElements.filter(function(element) {
+    return element > 500  && element % 2 === 0
+})
+console.log(filteredElements40)
+
 // 3.41. Gauti tik skaičius, kurie yra didesni už 1000 ir kurie dalinasi iš 3.
 // 3.42. Gauti tik skaičius, kurie yra mažesni už 1000 ir kurie dalinasi iš 9.
 // 3.43. Gauti tik skaičius, kurie yra mažesni už 500 ir kurie dalinasi iš 2.
